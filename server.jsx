75296c9de1728bb109da1d2fce8d63aa7be4f9b2
Abbas Yadollahi <abbas_y@hotmail.ca>
2018-11-17 16:12:54 -0500

    Remove api_key

--- server.jsx
+++ server.jsx
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'), () => {
   console.log('Listening on port ', server.address().port);
